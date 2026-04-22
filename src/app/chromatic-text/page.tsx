f054eaa81d611fc86d3def7dc7bb699b7ea90056
Stephenio Abbot <stephenioabbott@gmail.com>
2026-04-22 17:43:06 -0400

    added pages to display the example scenes

---
+++ src/app/chromatic-text/page.tsx
@@ -0,0 +1,9 @@
+import ChromaticTextScene from "@/examples/ChromaticTextScene/ChromaticTextScene.lazy";
+
+export default function ChromaticTextPage() {
+  return (
+    <div className="fullscreen">
+      <ChromaticTextScene />
+    </div>
+  );
+}
